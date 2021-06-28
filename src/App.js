884a667534df4f1eb15a77495098f4466b061f14
JosiCayon <josicayon93@gmail.com>
2021-06-28 08:23:30 +0200

    Ajuste de react y crear paginacion de la parte del negocio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Managment from './pages/Managment';
+
+function App() {
+  return (
+    <div className="App">
+ 
+    </div>
+  );
+}
+
+export default App;
